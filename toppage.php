c07c5ac6e4bb536256b821ff840ad46c822557b9
shirasu-m <84777784+shirasu-m@users.noreply.github.com>
2021-05-25 20:00:59 +0900

    Update toppage.php

--- toppage.php
+++ toppage.php
@@ -1,15 +1,8 @@
 <?php
 session_start();
 
-$error_msg = "";
-
-if(isset($_POST["login"])){
-	if(isset($_POST["id"])==true && $_POST["pwd"]=="abcd1234"){
-		$_SESSION["user_id"]=$_POST["id"];
-		$url = "toppage.php";
-		header("Location: {$url}");
-	} 
-	$error_msg = "IDまたはパスワードが正しく入力されていません。";
+if(isset($_SESSION["user_id"])==false){
+	$_SESSION["user_id"]="guest";
 }
 ?>
 
@@ -17,39 +10,64 @@ if(isset($_POST["login"])){
 <html lang="ja">
 	<head>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" href="login.css">
+		<link rel="stylesheet" href="toppage.css">
 		<title>XSS練習用</title>
 	</head>
-	
 	<body>
 		
 		<!--ヘッダー-->
 		<header>
-			<h1>サンプル用サイト</h1>
+			<h1>サンプルサイト</h1>
+			<p>ようこそ<?php echo $_SESSION["user_id"] ?>さん</p>
+			<nav id="global_navi">
+				<ul>
+					<li class="current"><a href="toppage.php">XSS</a></li>
+					<li><a href="file_upload.php">ファイルアップロード</a></li>
+					<li><a href="#">リンク3</a></li>
+					<li><a href="#">リンク4</a></li>
+					<li><a href="#">リンク5</a></li>
+				</ul>
+			</nav>
 		</header>
 		<!--ヘッダー-->
 		
-		<!--ログインフォーム-->
-		<div id="login_form">
-			<p class="login_title">Login form</p>
-			<form action="login.php" method="POST" name="login_form">
-				<div class="item">
-					<label class="item_label">ID</label>
-					<input class="item_input" type="text" name="id">
-				</div>
-				<div class="item">
-					<label class="item_label">Password</label>
-					<input class="item_input" type="password" name="pwd">
-				</div>
-				<div class="error">
-					<p><?php echo $error_msg; ?></p>
-				</div>
-				<div class="login_submit_wrap">
-					<input type="submit" name="login" value="LOGIN">
-				</div>
-			</form>	
+		<div id="container">
+			<!--メイン-->
+			<div id="main">
+				<h2>クロスサイトスクリプティング</h2>
+				<p id="contact_title">脆弱性に関するお問い合わせはこちら</p>
+				<section>
+					<form action="toppage_confirm.php" method="POST" id="contact_form">
+						<dl>
+							<dt>お名前</dt>
+							<dd><input class="contact_item" type="text" name="name" required></dd>
+							<dt>メールアドレス</dt>
+							<dd><input class="contact_item" type="email" name="email" required></dd>
+							<dt>問い合わせ内容</dt>
+							<dd><textarea name="detail" wrap="hard"  id="contact_detail"></textarea></dd>
+						</dl>
+						<div id="contact_submit_wrap">
+							<input type="submit" id="contact_submit_button" value="確認する">
+						</div>
+					</form>
+				</section>
+			</div>
+			<!--メイン-->
+			
+			<!--サイドバー-->
+			<aside id="sidebar">
+				<h2>関連リンク</h2>
+				<p>ここに何らかの関連リンクが入ります。</p>
+			</aside>
+			<!--サイドバー-->
+			
 		</div>
-		<!--ログインフォーム-->
+		
+		<!--フッター-->
+		<footer>
+			<small>2020 Sample footer</small>
+		</footer>
+		<!--フッター-->
 		
 	</body>
 </html>
